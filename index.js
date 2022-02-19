13bc3adc45f3b2c4879c2b7560f96e4b9d035365
Nima Azarbayjani <nimaza@gmail.com>
2022-02-19 20:32:21 +0100

    Setup test environment so that the server can get started/stoped by the tests as neeeded

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const config = require('./config');
-
 const app = require('./src/app');
 
-app.listen(config.PORT, () =>
+const server = app.listen(config.PORT, () =>
   console.log(`server listening on port ${process.env.PORT}`)
 );
+
+if (config.ENV === 'TEST') {
+  module.exports = server;
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,13 @@
 const axios = require('axios');
 
 const config = require('../config');
+const server = require('../index');
 
 describe('Test /live path', () => {
   test('GET /live responds with rlinks is live', async () => {
     const response = await axios.get(`${config.URL}/live`);
     expect(response.data).toBe('rlinks is live!');
   });
+afterAll(() => {
+  server.close();
 });
