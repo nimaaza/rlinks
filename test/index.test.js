aa4a8c3aa03a5e389c038b2ade8599b853d987e3
Nima Azarbayjani <nimaza@gmail.com>
2022-02-19 20:32:49 +0100

    More meaningful message for test block

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const config = require('../config');
 const server = require('../index');
 
-describe('Test /live path', () => {
+describe('Test basic backend behaviour', () => {
   test('GET /live responds with rlinks is live', async () => {
     const response = await axios.get(`${config.URL}/live`);
     expect(response.data).toBe('rlinks is live!');
