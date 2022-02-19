55a8f2c37baf73e9d1e9d33ae12ab59ab0890eeb
Nima Azarbayjani <nimaza@gmail.com>
2022-02-19 20:33:51 +0100

    Add test for fetching of index.html by means of GET /

--- test/index.test.js
+++ test/index.test.js
@@ -8,6 +8,18 @@ describe('Test basic backend behaviour', () => {
     const response = await axios.get(`${config.URL}/live`);
     expect(response.data).toBe('rlinks is live!');
   });
+
+  test('GET / responds with appropriate index.html file', async () => {
+    const response = await axios.get(`${config.URL}`);
+
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toContain('text/html');
+    expect(response.data).toContain(
+      '<title>RLinks - link reducer service</title>'
+    );
+  });
+});
+
 afterAll(() => {
   server.close();
 });
