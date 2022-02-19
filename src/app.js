426db2ba705fcb15fce7107086fcbc09db3827c2
Nima Azarbayjani <nimaza@gmail.com>
2022-02-19 20:34:53 +0100

    Send a basic index.html page in response to HTTP GET at /

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
-const config = require('../config');
 const express = require('express');
+const path = require('path');
+
+const config = require('../config');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 if (['DEV', 'TEST'].includes(config.ENV)) {
   app.get('/live', (request, response) => {
     response.status(200).send('rlinks is live!');
   });
 }
+
+app.get('/', (request, response) => {
+  response.sendFile('/index.html');
+});
 
 module.exports = app;
