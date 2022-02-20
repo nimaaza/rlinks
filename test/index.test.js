71e540af7ef6f809ca2ccdb2f968b4e3ef52f363
Nima Azarbayjani <nimaza@gmail.com>
2022-02-20 15:00:15 +0100

    Add basic tests for database operations

--- test/index.test.js
+++ test/index.test.js
@@ -35,6 +35,56 @@ describe('Test basic backend behaviour', () => {
   });
 });
 
+describe('Basic tests for the database', () => {
+  let sequelize, Link;
+
+  beforeAll(async () => {
+    ({ sequelize, Link } = require('../src/db'));
+  });
+
+  test('Connection to the test database is successful', async () => {
+    await sequelize.authenticate();
+    await sequelize.sync({ match: /-test$/ });
+  });
+
+  test('Basic CRUD operations on the Link model work', async () => {
+    const link = {
+      url: 'https://www.youtube.com/watch?v=LPLKOLJAbds',
+      shortKey: 'AbCdEFG',
+    };
+
+    const createdLink = await Link.create(link);
+    expect(createdLink.url).toEqual(link.url);
+    expect(createdLink.shortKey).toEqual(link.shortKey);
+    expect(createdLink.count).toBe(1);
+    expect(createdLink).toHaveProperty('createdAt');
+    expect(createdLink).toHaveProperty('updatedAt');
+    expect(createdLink).toHaveProperty('id');
+
+    const readLink = await Link.findOne({ where: { shortKey: link.shortKey } });
+    expect(readLink.url).toEqual(link.url);
+    expect(readLink.shortKey).toEqual(link.shortKey);
+    expect(readLink.count).toBe(1);
+    expect(readLink).toHaveProperty('createdAt');
+    expect(readLink).toHaveProperty('updatedAt');
+    expect(readLink).toHaveProperty('id');
+
+    await readLink.update({ shortKey: 'ABCDEFG' });
+    await readLink.increment({ count: 1 });
+    expect(readLink.shortKey).toEqual('ABCDEFG');
+    expect(readLink.count).toBe(2);
+
+    await readLink.destroy();
+    const allLinksInDb = await Link.findAll();
+    expect(allLinksInDb).toHaveLength(0);
+  });
+
+  afterAll(async () => {
+    await sequelize.drop({ match: /-test$/ });
+    await sequelize.close();
+  });
+});
+
 afterAll(() => {
   server.close();
 });
