5a7dafbd3ea3f7a24de1a6d8423b9d4ee86d7c7e
Nima Azarbayjani <nimaza@gmail.com>
2022-02-23 08:08:49 +0100

    Have the dummy front-end display the shortened link and number of times its created

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,17 +1,20 @@
 const form = document.querySelector('#form-url');
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', event => {
   event.preventDefault();
 
   const url = document.querySelector('#input-url').value;
+  const linkDisplay = document.querySelector('#created-link');
 
-  fetch('/', {
+  fetch('/shorten', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      url,
-    }),
-  });
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ url }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      const link = `http://localhost:3000/${data.shortKey}`;
+      const displayString = `Your shortened link is: <a href=${link}>${link}</a> and ${data.count} have created this link.`;
+      linkDisplay.innerHTML = displayString;
+    });
 });
