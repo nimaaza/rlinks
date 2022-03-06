6c00fd7313c3f6f4540a2640148eee42110abdac
Nima Azarbayjani <nimaza@gmail.com>
2022-03-06 21:48:03 +0100

    Avoid fetching site preview data when testing

--- rlinks-backend/src/helpers/previews.js
+++ rlinks-backend/src/helpers/previews.js
@@ -1,16 +1,25 @@
 const { getLinkPreview } = require('link-preview-js');
+const { ENV } = require('../../config');
 
-const getLinkPreviewData = async url => {
-  const data = await getLinkPreview(url);
+let getLinkPreviewData;
 
-  const { title, description } = data;
-  const image = data.images[0];
-
-  return {
-    title,
-    description,
-    image,
+if (ENV === 'TEST') {
+  getLinkPreviewData = async () => {
+    return { title: '', description: '', image: '' };
   };
-};
+} else {
+  getLinkPreviewData = async url => {
+    const data = await getLinkPreview(url);
+
+    const { title, description } = data;
+    const image = data.images[0];
+
+    return {
+      title,
+      description,
+      image,
+    };
+  };
+}
 
 module.exports = { getLinkPreviewData };
