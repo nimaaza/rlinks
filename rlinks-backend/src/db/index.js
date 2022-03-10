f192ebdf65c7f435f13e6bd023febd2961f492fc
Nima Azarbayjani <nimaza@gmail.com>
2022-03-10 21:57:04 +0100

    Restructure the database related code so that it can potentially be more modular

--- rlinks-backend/src/db/index.js
+++ rlinks-backend/src/db/index.js
@@ -1,9 +1,6 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const { ENV, DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_LOGGER } = require('../config');
-const { validUrl } = require('../helpers/url');
-const { getLinkPreviewData } = require('../helpers/previews');
-const { randomAlphaNumbericString } = require('../helpers/randomize');
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
@@ -17,87 +14,15 @@ const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   },
 });
 
-const Link = sequelize.define(
-  'Link',
-  {
-    url: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      unique: 'urlShortKeyCompositeIndex',
-    },
-    shortKey: {
-      type: DataTypes.STRING(7),
-      allowNull: false,
-      unique: 'urlShortKeyCompositeIndex',
-    },
-    title: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    image: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    count: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 1,
-    },
-    visits: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-    },
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-  },
-  {
-    timestamps: true,
-    indexes: [
-      { unique: true, fields: ['url'] },
-      {
-        name: 'id_index',
-        fields: [
-          'id',
-          {
-            name: 'id',
-            order: 'DESC',
-          },
-        ],
-      },
-    ],
-  }
-);
+const initializeModels = require('./models');
 
-Link.transformer = async url => {
-  if (!validUrl(url)) return;
-
-  const existingUrl = await Link.findOne({ where: { url } });
-
-  if (existingUrl) {
-    await existingUrl.increment({ count: 1 });
-    return existingUrl.dataValues;
-  } else {
-    const shortKey = randomAlphaNumbericString(7);
-    let { title, description, image } = await getLinkPreviewData(url);
-    const link = { url, shortKey, title, description, image };
-    const createdLink = await Link.create(link);
-    return createdLink.dataValues;
-  }
-};
+const { Link } = initializeModels(sequelize);
 
 const initDB = async () => {
   await sequelize.authenticate();
   await sequelize.sync();
 };
 
-initDB();
+if (['DEV', 'TEST', 'PROD'].includes(ENV)) initDB();
 
-module.exports = { sequelize, Link, initDB };
+module.exports = { sequelize, Link };
---
+++ rlinks-backend/src/db/models/link.js
@@ -0,0 +1,87 @@
+const { DataTypes } = require('sequelize');
+
+const { getLinkPreviewData } = require('../../helpers/previews');
+const { randomAlphaNumbericString } = require('../../helpers/randomize');
+const { validUrl } = require('../../helpers/url');
+
+const addLinkModel = sequelize => {
+  const Link = sequelize.define(
+    'Link',
+    {
+      url: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: 'urlShortKeyCompositeIndex',
+      },
+      shortKey: {
+        type: DataTypes.STRING(7),
+        allowNull: false,
+        unique: 'urlShortKeyCompositeIndex',
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      image: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      count: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+      },
+      visits: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+    },
+    {
+      timestamps: true,
+      indexes: [
+        { unique: true, fields: ['url'] },
+        {
+          name: 'id_index',
+          fields: [
+            'id',
+            {
+              name: 'id',
+              order: 'DESC',
+            },
+          ],
+        },
+      ],
+    }
+  );
+
+  Link.transformer = async url => {
+    if (!validUrl(url)) return;
+
+    const existingUrl = await Link.findOne({ where: { url } });
+
+    if (existingUrl) {
+      await existingUrl.increment({ count: 1 });
+      return existingUrl.dataValues;
+    } else {
+      const shortKey = randomAlphaNumbericString(7);
+      let { title, description, image } = await getLinkPreviewData(url);
+      const link = { url, shortKey, title, description, image };
+      const createdLink = await Link.create(link);
+      return createdLink.dataValues;
+    }
+  };
+
+  return Link;
+};
+
+module.exports = addLinkModel;
