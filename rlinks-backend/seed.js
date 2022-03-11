60a5aaa9dd8730f94dd0f5fe62128ef811d558e5
Nima Azarbayjani <nimaza@gmail.com>
2022-03-11 15:06:36 +0100

    Extract the array of sample URLs so they can be used in multiple places (for tests & seeding)

--- rlinks-backend/seed.js
+++ rlinks-backend/seed.js
@@ -1,31 +1,7 @@
 const { sequelize, Link } = require('./src/db');
 const { ENV } = require('./src/config');
 
-const sampleUrls = [
-  'https://sequelize.org/v6/manual/model-querying-basics.html#limits-and-pagination',
-  'https://sequelize.org/',
-  'https://join.com/companies/dot9/3960834-working-student-m-f-d-software-development',
-  'https://fullstackopen.com/en/part5/testing_react_apps',
-  'https://press.princeton.edu/books/paperback/9780691160887/addiction-by-design',
-  'https://testing-library.com/docs/react-testing-library/intro/',
-  'https://martinfowler.com/articles/practical-test-pyramid.html',
-  'https://kentcdodds.com/blog/how-to-add-testing-to-an-existing-project',
-  'https://www.albertgao.xyz/2017/05/24/how-to-test-expressjs-with-jest-and-supertest/',
-  'https://docs.cypress.io/guides/core-concepts/introduction-to-cypress#Cypress-Can-Be-Simple-Sometimes',
-  'https://www.w3.org/International/questions/qa-personal-names.en',
-  'https://support.microsoft.com/en-us/office/database-design-basics-eb2159cf-1e30-401a-8084-bd4f9c9ca1f5',
-  'https://ezdxf.readthedocs.io/en/stable/usage_for_beginners.html#arch-usr',
-  'https://doc.rust-lang.org/stable/book/',
-  'https://talks.osfc.io/osfc2021/talk/JTWYEH/',
-  'https://docs.docker.com/get-started/07_multi_container/',
-  'https://iep.utm.edu/truth/',
-  'https://www.youtube.com/watch?v=2Oe6HUgrRlQ',
-  'https://react-bootstrap.github.io/forms/select/',
-  'https://www.dw.com/en/top-stories/s-9097',
-  'https://keep.google.com/',
-  'https://www.jetbrains.com/',
-  'https://github.com/nimaaza/rlinks',
-];
+const urls = require('./data');
 
 const clear = async () => {
   await sequelize.authenticate();
@@ -35,8 +11,8 @@ const clear = async () => {
 
 const seed = async () => {
   await clear();
-  
-  sampleUrls.forEach(async url => {
+
+  urls.forEach(async url => {
     await Link.transformer(url);
   });
 };
--- rlinks-backend/test/index.test.js
+++ rlinks-backend/test/index.test.js
@@ -216,38 +216,14 @@ describe('Tests for end-point at /shorten for shortening links', () => {
 });
 
 describe('Tests for the end-point at /links for the pagination of produced short links', () => {
-  const sampleUrls = [
-    'https://sequelize.org/v6/manual/model-querying-basics.html#limits-and-pagination',
-    'https://sequelize.org/',
-    'https://www.dw.com/en/top-stories/s-9097',
-    'https://join.com/companies/dot9/3960834-working-student-m-f-d-software-development',
-    'https://keep.google.com/',
-    'https://www.jetbrains.com/',
-    'https://fullstackopen.com/en/part5/testing_react_apps',
-    'https://github.com/nimaaza/rlinks',
-    'https://press.princeton.edu/books/paperback/9780691160887/addiction-by-design',
-    'https://testing-library.com/docs/react-testing-library/intro/',
-    'https://martinfowler.com/articles/practical-test-pyramid.html',
-    'https://kentcdodds.com/blog/how-to-add-testing-to-an-existing-project',
-    'https://www.albertgao.xyz/2017/05/24/how-to-test-expressjs-with-jest-and-supertest/',
-    'https://docs.cypress.io/guides/core-concepts/introduction-to-cypress#Cypress-Can-Be-Simple-Sometimes',
-    'https://www.w3.org/International/questions/qa-personal-names.en',
-    'https://support.microsoft.com/en-us/office/database-design-basics-eb2159cf-1e30-401a-8084-bd4f9c9ca1f5',
-    'https://ezdxf.readthedocs.io/en/stable/usage_for_beginners.html#arch-usr',
-    'https://doc.rust-lang.org/stable/book/',
-    'https://talks.osfc.io/osfc2021/talk/JTWYEH/',
-    'https://docs.docker.com/get-started/07_multi_container/',
-    'https://iep.utm.edu/truth/',
-    'https://www.youtube.com/watch?v=2Oe6HUgrRlQ',
-    'https://react-bootstrap.github.io/forms/select/',
-  ];
+  const urls = require('../data');
   const limit = config.PAGINATION_LIMIT;
-  const paginationRequests = Math.floor(sampleUrls.length / limit);
+  const paginationRequests = Math.floor(urls.length / limit);
 
   beforeEach(async () => {
     await clearDB();
 
-    sampleUrls.forEach(async url => {
+    urls.forEach(async url => {
       await Link.transformer(url);
     });
   });
@@ -282,7 +258,7 @@ describe('Tests for the end-point at /links for the pagination of produced short
     }
 
     const lastPage = await doAxiosPost('links', { mode, cursor });
-    expect(lastPage.data.links).toHaveLength(sampleUrls.length - paginationRequests * limit);
+    expect(lastPage.data.links).toHaveLength(urls.length - paginationRequests * limit);
     expect(lastPage.data.hasNext).toBe(false);
     testPaginationOrder(lastPage.data.links, mode);
   };
