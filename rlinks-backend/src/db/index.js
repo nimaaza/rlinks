41428f9fe7bd7db5659c42d877bd1a21316bd3bf
Nima Azarbayjani <nimaza@gmail.com>
2022-03-26 12:01:46 +0100

    Log messages when connecting to the database

--- rlinks-backend/src/db/index.js
+++ rlinks-backend/src/db/index.js
@@ -1,6 +1,10 @@
 const { Sequelize } = require('sequelize');
 
+const logger = require('../helpers/logger');
+const initializeModels = require('./models');
 const { ENV, DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_LOGGER } = require('../config');
+
+logger(`Connecting to PostgreSQL server at ${DB_HOST}:${DB_PORT}`);
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
@@ -15,6 +19,7 @@ const { Link, User } = initializeModels(sequelize);
 const connect = async () => {
   await sequelize.authenticate();
   await sequelize.sync({ alter: ENV === 'TEST' });
+  logger('Connection to database established.');
 };
 
 if (['DEV', 'TEST', 'PROD'].includes(ENV)) connect();
