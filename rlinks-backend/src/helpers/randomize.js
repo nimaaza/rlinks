a800960766f0f33e89b3f8cce76d14c2e055f76c
Nima Azarbayjani <nimaza@gmail.com>
2022-04-27 16:42:51 +0200

    Minor code simplifications

--- rlinks-backend/src/helpers/randomize.js
+++ rlinks-backend/src/helpers/randomize.js
@@ -1,19 +1,16 @@
-const randomIntUpTo = n => Math.floor(Math.random() * n);
+const randomIntegerUpTo = n => Math.floor(Math.random() * n);
 
 const randomAlphaNumbericString = n => {
-  const acceptableCharacters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const length = acceptableCharacters.length;
+  const acceptableCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const numberOfAcceptableCharacters = acceptableCharacters.length;
   let randomString = '';
 
   for (let i = 0; i < n; i++) {
-    const randomCharacter = acceptableCharacters.charAt(randomIntUpTo(length));
+    const randomCharacter = acceptableCharacters.charAt(randomIntegerUpTo(numberOfAcceptableCharacters));
     randomString += randomCharacter;
   }
 
   return randomString;
 };
 
-module.exports = {
-  randomAlphaNumbericString,
-};
+module.exports = { randomAlphaNumbericString };
