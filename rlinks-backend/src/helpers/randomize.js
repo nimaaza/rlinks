d8b4bea5e8f18f03d911544b2cfe6d52c07f362d
Nima Azarbayjani <nimaza@gmail.com>
2022-05-09 16:45:34 +0200

    refactoring/Rename variable

--- rlinks-backend/src/helpers/randomize.js
+++ rlinks-backend/src/helpers/randomize.js
@@ -1,12 +1,12 @@
 const randomIntegerUpTo = n => Math.floor(Math.random() * n);
 
 const randomAlphaNumbericString = n => {
-  const acceptableCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const numberOfAcceptableCharacters = acceptableCharacters.length;
+  const alphaNumerics = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const numberOfAcceptableCharacters = alphaNumerics.length;
   let randomString = '';
 
   for (let i = 0; i < n; i++) {
-    const randomCharacter = acceptableCharacters.charAt(randomIntegerUpTo(numberOfAcceptableCharacters));
+    const randomCharacter = alphaNumerics.charAt(randomIntegerUpTo(numberOfAcceptableCharacters));
     randomString += randomCharacter;
   }
 
